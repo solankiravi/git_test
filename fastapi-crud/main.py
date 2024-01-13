fc1c2b73646f97ee56aa24480b691edfd7aa24ab
Ravi Kumar <ravikumarsinghsolanki@gmail.com>
2024-01-13 21:35:57 +0530

    fix: linter issue

--- fastapi-crud/main.py
+++ fastapi-crud/main.py
@@ -7,19 +7,23 @@ app = FastAPI()
 # def index():
 #     return {"blog_list": []}
 
+
 @app.get("/about")
 def about():
     return {"page": "about page"}
+
 
 @app.get("/blog/{id}")
 def blog_by_id(id: int):
     return {"blog": id}
 
+
 @app.get("/")
-def get_first_n_blogs(limit: int = 20, published: bool = True, sort: Optional[str]=None):
+def get_first_n_blogs(limit: int = 20, published: bool = True, sort: Optional[str] = None):
     if published:
         return {"data": f"first {limit} published blogs from blog_list"}
     return {"data": f"first {limit} unpublished blogs from blog_list"}
+
 
 @app.post("/")
 def create_blog(item: Item):
--- fastapi-crud/models.py
+++ fastapi-crud/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     id: int
     name: str
