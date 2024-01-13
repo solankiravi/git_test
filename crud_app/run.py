3b91a5a889bb5a3ff0b693f982b3b6da19d7fe90
Ravi Kumar <ravikumarsinghsolanki@gmail.com>
2024-01-13 21:00:02 +0530

    Added flake8 support

--- crud_app/run.py
+++ crud_app/run.py
@@ -5,6 +5,7 @@ import uvicorn
 from .main import app
 from . import __version__
 
+
 @app.get("/hello")
 def read_root():
     return {"Version": __version__}
@@ -14,9 +15,11 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 def start():
     """Launched with `poetry run start` at root level"""
     uvicorn.run("crud_app.run:app", host="0.0.0.0", port=8000, reload=True)
+
 
 if __name__ == "__main__":
     start()
