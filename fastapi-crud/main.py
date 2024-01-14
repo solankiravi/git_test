17315cc93119f7e40fe4ee581520775f28317ac7
Ravi Kumar <ravikumarsinghsolanki@gmail.com>
2024-01-14 20:28:55 +0530

    Day 2: blog app

--- fastapi-crud/main.py
+++ fastapi-crud/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from typing import Optional
 from .models import Item
+from blog.models import Blog
+
+
 app = FastAPI()
 
 # @app.get("/")
 # def index():
 #     return {"blog_list": []}
+
+
+@app.post("/")
+def create(blog: Blog):
+    return blog
 
 
 @app.get("/about")
@@ -19,7 +27,9 @@ def blog_by_id(id: int):
 
 
 @app.get("/")
-def get_first_n_blogs(limit: int = 20, published: bool = True, sort: Optional[str] = None):
+def get_first_n_blogs(
+    limit: int = 20, published: bool = True, sort: Optional[str] = None
+):
     if published:
         return {"data": f"first {limit} published blogs from blog_list"}
     return {"data": f"first {limit} unpublished blogs from blog_list"}
